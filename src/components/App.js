19c1f97ab3886f10e1a3e4c848185395260a915d
sanvean74 <dlambertpdx@gmail.com>
2019-10-30 09:46:53 -0700

    fixed deleted stuff

--- src/components/App.js
+++ src/components/App.js
@@ -2,46 +2,49 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Character from './Character';
+import styles from './App';
 
 const App = () => (
   <>
     <Header />
-    <Character
-      img="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-      name="Rick Sanchez"
-      species="Human"/>
-    <Character
-      img="https://rickandmortyapi.com/api/character/avatar/5.jpeg"
-      name="Jerry Smith"
-      species="Human"/>
-    <Character
-      img="https://rickandmortyapi.com/api/character/avatar/22.jpeg"
-      name="Aqua Rick"
-      species="Fish-Person"/>
-    <Character
-      img="https://rickandmortyapi.com/api/character/avatar/3.jpeg"
-      name="Summer Smith"
-      species="Human"/>
-    <Character
-      img="https://rickandmortyapi.com/api/character/avatar/363.jpeg"
-      name="Trandor"
-      species="Alien"/>
-    <Character
-      img="https://rickandmortyapi.com/api/character/avatar/190.jpeg"
-      name="Keara"
-      species="Alien"/>
-    <Character
-      img="https://rickandmortyapi.com/api/character/avatar/421.jpeg"
-      name="Rich Plutonian"
-      species="Alien"/>
-    <Character
-      img="https://rickandmortyapi.com/api/character/avatar/88.jpeg"
-      name="Cynthia"
-      species="Human"/>
-    <Character
-      img="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-      name="Morty Smith"
-      species="Human"/>
+    <div>
+      <Character
+        img="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+        name="Rick Sanchez"
+        species="Human"/>
+      <Character
+        img="https://rickandmortyapi.com/api/character/avatar/5.jpeg"
+        name="Jerry Smith"
+        species="Human"/>
+      <Character
+        img="https://rickandmortyapi.com/api/character/avatar/22.jpeg"
+        name="Aqua Rick"
+        species="Fish-Person"/>
+      <Character
+        img="https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+        name="Summer Smith"
+        species="Human"/>
+      <Character
+        img="https://rickandmortyapi.com/api/character/avatar/363.jpeg"
+        name="Trandor"
+        species="Alien"/>
+      <Character
+        img="https://rickandmortyapi.com/api/character/avatar/190.jpeg"
+        name="Keara"
+        species="Alien"/>
+      <Character
+        img="https://rickandmortyapi.com/api/character/avatar/421.jpeg"
+        name="Rich Plutonian"
+        species="Alien"/>
+      <Character
+        img="https://rickandmortyapi.com/api/character/avatar/88.jpeg"
+        name="Cynthia"
+        species="Human"/>
+      <Character
+        img="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+        name="Morty Smith"
+        species="Human"/>
+    </div>
     <Footer />
   </>
 );
